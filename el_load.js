31534d278dd3cc419ed5412766daec2dbabb6a85
ELAND\benchien <benchien@eland.com.tw>
2023-07-19 16:29:35 +0800

    更新版本號

--- el_load.js
+++ el_load.js
@@ -1,7 +1,7 @@
 var ElandTracker = ElandTracker || {};
 
 ElandTracker.options = {
-    "elandTracker": "//cdn.jsdelivr.net/gh/yEchKgnaHWFO/eland-tracker@4.44/",
+    "elandTracker": "//cdn.jsdelivr.net/gh/yEchKgnaHWFO/eland-tracker@4.45/",
     "elandReceiver": "//dmp.eland-tech.com/dmpreceiver/",
     "useJsdelivrToLoadJS": true,
     "useFingerprint": false
