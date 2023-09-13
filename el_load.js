6b31d1f6705e098ed8eaf2321e7332f161dd1d07
ELAND\benchien <benchien@eland.com.tw>
2023-09-13 17:08:05 +0800

    no message

--- el_load.js
+++ el_load.js
@@ -1,7 +1,7 @@
 var ElandTracker = ElandTracker || {};
 
 ElandTracker.options = {
-    "elandTracker": "//cdn.jsdelivr.net/gh/yEchKgnaHWFO/eland-tracker@4.6.2/",
+    "elandTracker": "//cdn.jsdelivr.net/gh/yEchKgnaHWFO/eland-tracker@4.6.3/",
     "elandReceiver": "//dmp.eland-tech.com/dmpreceiver/",
     "useJsdelivrToLoadJS": true,
     "useFingerprint": false
